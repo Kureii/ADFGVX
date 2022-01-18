406df0dfd730e7b0450e1d0c8e1885e1f9a69c22
Tomáš Adámek <ad.tomik@seznam.cz>
2022-01-18 22:59:42 +0100

    finish 5x5 start 6x6

---
+++ Lang.py
@@ -0,0 +1,47 @@
+cs_CZ = {
+    "nameRandom": "Náhodně",
+    "nameKey1": "Klíč číslo 1",
+    "nameKey2": "Klíč číslo 2",
+    "nameTypeHere": "PIŠTE ZDE",
+    "nameChoseFile": "Vyberte soubor",
+    "nameFile": "Soubor",
+    "nameFileUnchosed": "Soubor nevybrán",
+    "nameSpcRpl": "Nahrazující znak",
+    "nameEncode": "Šifrovat",
+    "nameDecode": "Dešifrovat",
+    "nameSpecChar": "Znak k nahrazení mezer: ",
+    "nameWhyEnDe": "Musíte vyplnit všechny kolonky pro zpřístupnění tohoto tlačítka",
+    "nameEmpty": "Soubor je prázný.",
+    "nameEncodeTxt": "Zašifrovaný text",
+    "nameOpenText": "Otevřený text",
+    "nameSaveTxt": "Uložit text",
+    "nameSaveKeys": "Uložit klíče",
+    "nameSlctTXT": "Textový soubor (*.txt)",
+}
+eng_US = {
+    "nameRandom": "Random",
+    "nameKey1": "Key number 1",
+    "nameKey2": "Key number 2",
+    "nameTypeHere": "TYPE HERE",
+    "nameChoseFile": "Chose file",
+    "nameFile": "File",
+    "nameFileUnchosed": "File unchosed",
+    "nameSpcRpl": "Supplement characer",
+    "nameEncode": "Encode",
+    "nameDecode": "Decode",
+    "nameSpecChar": "Char to replace spaces: ",
+    "nameWhyEnDe": "You must fill all field for endable this button",
+    "nameEmpty": "File is empty.",
+    "nameEncodeTxt": "Encoded text",
+    "nameOpenText": "Open text",
+    "nameSaveTxt": "Save text",
+    "nameSaveKeys": "Save keys",
+    "nameSlctTXT": "Text file (*.txt)",
+}
+
+
+appLang = {
+    "cs_CZ": cs_CZ,
+    "eng_US": eng_US
+}
+
